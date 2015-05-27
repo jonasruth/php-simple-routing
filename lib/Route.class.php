64bd95b590a706338e353490825074b4cba8b866
jonasruth <jonasruth@gmail.com>
2015-05-27 02:29:55 -0300

    Correções

--- lib/Route.class.php
+++ lib/Route.class.php
@@ -1,7 +1,8 @@
 <?php
 
-class RouteNotFoundException extends Exception {
-    
+class RouteNotFoundException extends Exception
+{
+
 }
 
 class Route
@@ -15,11 +16,15 @@ class Route
     private $params;
     private $_protocol;
     private $_host;
-    private $_basedir;    
+    private $_basedir;
 
-    public function __Construct(){ }
+    public function __Construct()
+    {
+    }
 
-    private function __Clone(){}    
+    private function __Clone()
+    {
+    }
 
     public static function getInstance()
     {
@@ -28,20 +33,22 @@ class Route
         }
         return self::$_instance;
     }
-    
-    public function setConfig($routeList,$host,$basedir,$protocol='http'){
-        
+
+    public function setConfig($routeList, $host, $basedir, $protocol = 'http')
+    {
+
         $this->routeList = $routeList;
         $this->_protocol = $protocol;
-        $this->_host     = $host;
-        $this->_basedir  = $basedir;
-        
+        $this->_host = $host;
+        $this->_basedir = $basedir;
+
         return $this;
-        
+
     }
 
-    public function init($url){
-        
+    public function init($url)
+    {
+
         $this->url = $url;
         $this->params = new stdClass;
         $this->urlNormalize()->routePatternMake();
@@ -49,38 +56,38 @@ class Route
         return $this;
     }
 
-    protected function urlNormalize(){
+    protected function urlNormalize()
+    {
 
-        if(preg_match('/'. str_replace('/','\/',$this->_basedir) .'([a-z_-]+\.php)?(.*)/',$this->url,$matches)){
+        if (preg_match('/' . str_replace('/', '\/', $this->_basedir) . '([a-z_-]+\.php)?(.*)/', $this->url, $matches)) {
             $this->url = $matches[2];
-            $this->url = '/'.trim($this->url,'/');
-        }else throw new Exception('URL inválida.');
-        
+            $this->url = '/' . trim($this->url, '/');
+        } else throw new Exception('URL inválida.');
+
         return $this;
 
     }
 
     public function routePatternMake()
     {
-        foreach($this->routeList as $rulename=>$rules)
-        {
+        foreach ($this->routeList as $rulename => $rules) {
             $rule = $rules['rule'];
-            $rule = preg_replace(array('/\//','/\./'),array('\/','\.'),$rule);            
+            $rule = preg_replace(array('/\//', '/\./'), array('\/', '\.'), $rule);
             $count = 0;
-            while(preg_match('/\{([\w_]+)\}/',$rule,$matches)){
+            while (preg_match('/\{([\w_]+)\}/', $rule, $matches)) {
                 $count++;
 
-                if(!isset( $rules['params'][$matches[1]]['pattern'] )){                    
+                if (!isset($rules['params'][$matches[1]]['pattern'])) {
                     $this->routeList[$rulename]['params'][$matches[1]]['pattern'] = '[\w:-]+'; // copia para a real 
                 }
                 $this->routeList[$rulename]['params_order'][] = $matches[1];
-                $rule = preg_replace('/\{'.$matches[1].'\}/','('.$this->routeList[$rulename]['params'][$matches[1]]['pattern'].')',$rule);
-                
-                if($count > 100) exit;
+                $rule = preg_replace('/\{' . $matches[1] . '\}/', '(' . $this->routeList[$rulename]['params'][$matches[1]]['pattern'] . ')', $rule);
+
+                if ($count > 100) exit;
             }
             $rule = "/^{$rule}$/";
             $this->routeList[$rulename]['pattern'] = $rule;
-        }        
+        }
 
         return $this;
     }
@@ -96,29 +103,30 @@ class Route
         $matched = false;
         $url = $this->url;
         $query_string = '';
-        if(strpos($this->url,'?')){
-            $url = strstr($this->url,'?',true);
-            $query_string = strstr($this->url,'?',false);
+        if (strpos($this->url, '?')) {
+            $url = strstr($this->url, '?', true);
+            $query_string = strstr($this->url, '?', false);
         }
 
-        foreach($this->routeList as $rulename=>$rules){
-    
-            if(preg_match($rules['pattern'],$url,$matches)){      
+        foreach ($this->routeList as $rulename => $rules) {
+
+            if (preg_match($rules['pattern'], $url, $matches)) {
 
                 $this->matchedRoute = $rules;
                 $this->matchedRoute['routename'] = $rulename;
-                if(is_array($rules['params']))
-                {
-                    foreach($rules['params_order'] as $param_order=>$param_name)
-                    {
-                        $this->params->{$param_name} = $_GET[$param_name] = $matches[$param_order+1];
+                if (
+                    (isset($rules['params']) && is_array($rules['params'])) &&
+                    (isset($rules['params_order']) && is_array($rules['params_order']))
+                ) {
+                    foreach ($rules['params_order'] as $param_order => $param_name) {
+                        $this->params->{$param_name} = $_GET[$param_name] = $matches[$param_order + 1];
                     }
                 }
 
-                if(!empty($query_string)){
-                    $pairs = explode('&',ltrim($query_string,'?'));
-                    foreach($pairs as $pair){
-                        list($key,$value) = explode('=',$pair);
+                if (!empty($query_string)) {
+                    $pairs = explode('&', ltrim($query_string, '?'));
+                    foreach ($pairs as $pair) {
+                        list($key, $value) = explode('=', $pair);
                         $this->params->{$key} = $_GET[$key] = $value;
                     }
                 }
@@ -127,39 +135,44 @@ class Route
             }
         }
 
-        if(!$matched)throw new RouteNotFoundException('Rota não existente.');
+        if (!$matched) throw new RouteNotFoundException('Rota não existente.');
 
         return $this;
     }
 
-    public function createLink($routeName,$params=array()){
-        $ruleCopy = $this->routeList[$routeName]['rule'];        
-        if(is_array($this->routeList[$routeName]['params'])){
-            foreach($this->routeList[$routeName]['params'] as $one_param_key=>$one_param_value){
-                $ruleCopy = preg_replace('/(\{' . $one_param_key . '\})/',$params[$one_param_key],$ruleCopy);
+    public function createLink($routeName, $params = array())
+    {
+        $ruleCopy = $this->routeList[$routeName]['rule'];
+        if (isset($this->routeList[$routeName]['params']) && is_array($this->routeList[$routeName]['params'])) {
+            foreach ($this->routeList[$routeName]['params'] as $one_param_key => $one_param_value) {
+                $ruleCopy = preg_replace('/(\{' . $one_param_key . '\})/', $params[$one_param_key], $ruleCopy);
             }
         }
-        return $this->_protocol.'://'.$this->_host.$this->_basedir.trim($ruleCopy,'/');
+        return $this->_protocol . '://' . $this->_host . $this->_basedir . trim($ruleCopy, '/');
     }
 
-    public function getParams(){
+    public function getParams()
+    {
         return $this->params;
     }
 
-    public function getParam($paramName){
-        if(in_array(array_keys($this->params))) return $this->params[$paramName];
-        return null;
+    public function getParam($paramName)
+    {
+        return $this->params->$paramName;
     }
 
-    public function getMatchedRouteName(){
+    public function getMatchedRouteName()
+    {
         return $this->matchedRoute['routename'];
     }
-    
-    public function getMatchedRouteAction(){
+
+    public function getMatchedRouteAction()
+    {
         return $this->matchedRoute['action'];
     }
 
-    public function getMatchedRoute(){
+    public function getMatchedRoute()
+    {
         return $this->matchedRoute;
     }
 
